a1b3069b99ae26d82c0da5f4d0805b3dae703eac
Rustem <rustem080@yandex.ru>
2019-02-24 22:41:37 +0300

    Внесены изменения

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        Array arr = new Array(10000);
+
+
+        arr.setArr();
+        System.out.println(arr.getLength());
+        float lastTime = System.nanoTime();
+        arr.sortBubble();
+        float realTime = System.nanoTime();
+        float deltaTime = (realTime - lastTime) / 100000000;
+        System.out.println(deltaTime);
+
+        arr.setArr();
+        float lastTime2 = System.nanoTime();
+        arr.sortSelect();
+        float realTime2 = System.nanoTime();
+        float deltaTime2 = (realTime2 - lastTime2) / 100000000;
+        System.out.println(deltaTime2);
+
+        arr.setArr();
+        float lastTime3 = System.nanoTime();
+        arr.sortInsert();
+        float realTime3 = System.nanoTime();
+        float deltaTime3 = (realTime3 - lastTime3) / 100000000;
+        System.out.println(deltaTime3);
+    }
+}
